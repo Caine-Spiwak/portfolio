709e34e49dbfc6c19f9cd5077cb7b89c3834a043
Caine Spiwak <cainespiwak@gmail.com>
2024-02-09 09:35:00 -0800

    minor bug fix

--- src/sendMail.js
+++ src/sendMail.js
@@ -8,7 +8,6 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
     user: process.env.GMAIL_ACC,
     pass: process.env.GMAIL_PASS,
   },
@@ -30,12 +29,6 @@ async function main() {
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
 }
 
 main().catch(console.error);
