c38c60e91de1aee2d58b210b8c03cc809657a0ff
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-02-10 13:04:57 -0800

    vite configure proxy

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // any route that is not api will be redirected to index.html
   app.get('*', (req, res) => 
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
